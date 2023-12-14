b61d90e737c486a7a015f62d9f9d536b3a03383e
Akash CS <84382172+akash-cs13@users.noreply.github.com>
2023-12-14 09:34:32 +0100

    loop for charge cars

--- charging-station/src/com/fhdo/Main.java
+++ charging-station/src/com/fhdo/Main.java
@@ -36,17 +36,11 @@ public class Main {
 		// try : \\res\\inputs\\Cars.txt or \\charging-station\\res\\inupts\\Cars.txt
 	    List<Car> cars = reader.readFile("\\charging-station\\res\\inupts\\Cars.txt");
 
-		// Create cars
-		Car car1 = cars.get(0);
-		Car car2 = cars.get(1);
-		Car car3 = cars.get(2);
-		Car car4 = cars.get(3);
-		
 		// Charge cars at the charging station
-		chargingStationManager.addCarToChargingStation(car1);	
-		chargingStationManager.addCarToChargingStation(car2);
-		chargingStationManager.addCarToChargingStation(car3);
-		chargingStationManager.addCarToChargingStation(car4);
+		for (Car car : cars) {
+			chargingStationManager.addCarToChargingStation(car);
+		}
+
 		
 		// Handle waiting list in a separate thread
 		chargingStationManager.handleWaitingList();
